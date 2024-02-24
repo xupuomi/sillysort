50d8e8bb57c1080e79a27f997b033d838bfb2a1b
naomilin <89211082+naomiyanchenlin@users.noreply.github.com>
2024-02-24 16:18:26 -0600

    basic framework done, return directory left

---
+++ frontend/renderer/js/renderer.js
@@ -0,0 +1,8 @@
+// const outputPath = document.querySelector('#output-path');
+// const filename = document.querySelector('#filename');
+
+// function loadFile(e) {
+//     const file = e.target.files[0];
+// }
+
+// img.addEventListener('change', loadFile);
