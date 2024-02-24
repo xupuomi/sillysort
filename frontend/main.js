7f2192e26fb7ec35bd65324decfbbc98fc49f9d2
naomilin <89211082+naomiyanchenlin@users.noreply.github.com>
2024-02-24 13:09:23 -0600

    dev mode

--- frontend/main.js
+++ frontend/main.js
@@ -1,18 +1,29 @@
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
 
+const isDev = process.env.NODE_ENV !== 'production';
 const isMac = process.platform === 'darwin';
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'SILLYsort',
-        width: 500,
+        width: isDev ? 1000 : 500,
         height: 600
     });
+
+    //Open devtools if in dev env
+    if(isDev){
+        mainWindow.webContents.openDevTools();
+    }
 
     mainWindow.loadFile(path.join(__dirname, 'renderer/index.html'));
 }
 app.whenReady().then(() => {
     createMainWindow();
+    app.on('activate', () => {
+        if(BrowserWindow.getAllWindows().length === 0){
+            createMainWindow();
+        }
+    });
 });
 
 app.on('window-all-closed', () => {
