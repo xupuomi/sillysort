df46b045eb5e2ed188b86b9671ede0f958c6e539
naomilin <89211082+naomiyanchenlin@users.noreply.github.com>
2024-02-24 16:39:27 -0600

    wop

--- frontend/renderer/js/renderer.js
+++ frontend/renderer/js/renderer.js
@@ -6,3 +6,16 @@
 // }
 
 // img.addEventListener('change', loadFile);
+
+document.getElementById("filepicker").addEventListener(
+    "change",
+    (event) => {
+      let output = document.getElementById("listing");
+      for (const file of event.target.files) {
+        let item = document.createElement("li");
+        item.textContent = file.webkitRelativePath;
+        output.appendChild(item);
+      }
+    },
+    false,
+  );
