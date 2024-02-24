ee0311680f9ddf23318c0abb0a5d5c998d5bc589
naomilin <89211082+naomiyanchenlin@users.noreply.github.com>
2024-02-24 12:16:56 -0600

    quick save before merge

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const { app, BrowserWindow } = require('electron');
 
+const isMac = process.platform === 'darwin';
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'SILLYsort',
@@ -7,5 +9,14 @@ function createMainWindow() {
         height: 600
     });
 
-    mainWindow.loadFile();
+    mainWindow.loadFile(path.join(_dirname, './renderer/index.html'));
 }
+app.whenReady().then(() => {
+    createMainWindow();
+});
+
+app.on('window-all-closed', () => {
+    if(!isMac){
+        app.quit()
+    }
+})
