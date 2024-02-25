4f192e41cff889a6499e62543e9b567ee6360149
KaustubhKhulbe <kaustubh.khulbe@gmail.com>
2024-02-24 22:11:06 -0600

    mostly working

--- backend/generate_commands.js
+++ backend/generate_commands.js
@@ -4,14 +4,21 @@ import { generateJson } from './llm_grouper.js';
 
 var commands = []
 
-generateOutput("/Users/kaustubhkhulbe/Downloads/");
+// fs.readFile("/Users/kaustubhkhulbe/Documents/2024/CS/SillySort/backend/data.json", function (error, content) {
+//     var data = JSON.parse(content);
+//     // console.log(data);
+//     const absolutePath = "User/....";
+//     allCommands(data, "", absolutePath);
+//     console.log(commands)
+// });
+
+generateOutput("/Users/kaustubhkhulbe/Downloads");
 
 async function generateOutput(filePath) {
     const json = readDir(filePath);
     var res = await generateJson(json);
-    allCommands(res);
-    printCommands();
-    // console.log(commands);
+    allCommands(res, "", filePath);
+    console.log(commands);
 }
 
 function readDir(dir) {
@@ -24,13 +31,6 @@ function readDir(dir) {
         }
             
     }
-
-    // fs.writeFile("/frontend/out.json", JSON.stringify(fileNameObj), function(err) {
-    //     if (err) {
-    //         console.log(err);
-    //     }
-    // });
-    // console.log("Started.");
 
     return fileNameObj
 }
@@ -56,29 +56,29 @@ function readFilesSync(dir) {
     return files;
   }
 
-function allCommands(data) {
+function allCommands(data, curr_path, absolutePath) {
     // console.log(data)
     for (const [key, value] of Object.entries(data)) {
-        console.log(key + ", " + value)
-        if (key != "Delete") addCommands(key, value)
-        else {
-            deleteCommands(value)
+        if (!Array.isArray(value)) {
+            // console.log(key + ",fsd " + value)
+            allCommands(value, curr_path + "/" + key, absolutePath)
         }
+        else addCommands(key, value, curr_path, absolutePath)
     }
 }
 
-function addCommands(name, data) {
-    commands.push(`mkdir ${name}`)
+function addCommands(name, data, curr_path, absolutePath) {
+    commands.push(`mkdir -p ${curr_path}/${name}`)
     for (var i = 0; i < data.length; i++) {
-        commands.push(`mv ${data[i]} ./${name}/${data[i]}`)
+        commands.push(`mv ${absolutePath}/${data[i]} ${absolutePath}/${curr_path}/${name}/${data[i]}`)
     }
 }
 
-function deleteCommands(data) {
-    for (var i = 0; i < data.length; i++) {
-        commands.push(`rm -rf ${data[i]}`)
-    }
-}
+// function deleteCommands(data, curr_path) {
+//     for (var i = 0; i < data.length; i++) {
+//         commands.push(`rm -rf ${data[i]}`)
+//     }
+// }
 
 function printCommands() {
     for (var i = 0; i < commands.length; i++) {
