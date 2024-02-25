ee3ad49b0fd0920857a660eb55c9523efef0c29e
KaustubhKhulbe <kaustubh.khulbe@gmail.com>
2024-02-24 20:33:03 -0600

    started hooking up

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
+const exec = require('child_process').exec;
 
 const isDev = process.env.NODE_ENV !== 'production';
 const isMac = process.platform === 'darwin';
@@ -8,6 +9,17 @@ function createMainWindow() {
         title: 'SILLYsort',
         width: isDev ? 1000 : 500,
         height: 600
+    });
+
+    // cp.execFile('node', path.join(__dirname, '../backend/test.js'));
+    // console.log("HERE");
+    const child = exec('node ../backend/generate_commands.js',
+        (error, stdout, stderr) => {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
     });
 
     //Open devtools if in dev env
