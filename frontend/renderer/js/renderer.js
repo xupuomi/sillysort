ec1512a1662e7b7d7248a3807f2da35bec085bcc
kevskizhang <kevin.yiyu.zhang@gmail.com>
2024-02-24 21:35:33 -0600

    Finally finsihed the fucking get directory function super scuffed

--- frontend/renderer/js/renderer.js
+++ frontend/renderer/js/renderer.js
@@ -34,4 +34,31 @@ function getAbsolutePaths() {
     }
     console.log(absolutePaths);
     return absolutePaths;
-}    
+}
+
+function getDirectory() {
+    let files = document.getElementById("filepicker").files;
+    let fullDirectory = files[0].path;
+    // console.log(fullDirectory);
+    // fullDirectory = fullDirectory.replace(/"\\"/g, "/");
+    // console.log(fullDirectory);
+    // //let relativeDirectory = fullDirectory.substring(0, fullDirectory.lastIndexOf("\\"));
+    // let relativeDirectory = files[0].webkitRelativePath;
+    // console.log(relativeDirectory);
+    // directory = fullDirectory.replace(relativeDirectory, "");
+    // console.log(directory);
+    // return directory;
+    let relativeDirectory = files[0].webkitRelativePath;
+    let relativeDirectories = relativeDirectory.split("/");
+    directories = fullDirectory.split("\\");
+    // console.log(directories);
+    // console.log("Hello World");
+    // console.log(relativeDirectories[0]);
+    while (directories[directories.length - 1] != [relativeDirectories[0]]) {
+        directories.pop();
+    }
+    // console.log(directories);
+    let finalDirectory = directories.join("/");
+    // console.log(finalDirectory);
+    return finalDirectory;
+}
